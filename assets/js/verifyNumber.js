76d128741fa7810ed587223eff98cc7cadc7fdff
isaaccodekill <isaacbello3200@gmail.com>
2019-07-30 14:57:07 +0100

    Finished final corrections

--- assets/js/verifyNumber.js
+++ assets/js/verifyNumber.js
@@ -1,5 +1,8 @@
 const verifyButton = document.querySelector('.Verification-submit')
+const failedImage = document.querySelector('.failed-img')
 const successImage = document.querySelector('.success-image')
+const imgCont = document.querySelector('.notify-image-container')
+const closeImage = document.querySelector('.close-img')
 const overlay = document.querySelector('.overlay')
 const input = document.querySelector('.Verification-input')
 
@@ -8,7 +11,9 @@ verifyButton.addEventListener('click', (e) => {
 	if(input.value !== ''){
 		e.preventDefault()
     overlay.classList.add('backgroundBlend')
-    successImage.classList.add('show-success')	
+    successImage.classList.add('showSuccess')
+    closeImage.classList.add("showSuccess")	
+    imgCont.classList.add('showSuccess')
 	}
     
 })
