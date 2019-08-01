38b990a1eb834260ecfc7603443809ec9356ee2b
Nifemi <deyeminifemi@gmail.com>
2019-08-01 21:38:28 +0100

    Added dropdown to signup page

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -4,3 +4,12 @@ document.querySelectorAll('.password-div').forEach(e => {
         passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password'
     })
 });
+
+[...document.querySelectorAll('.select-option')].forEach(e => {
+    e.addEventListener('click', () => {
+        const parent = e.parentElement;
+        const target = parent.dataset.target;
+        document.getElementById(target).value = e.innerText;
+        parent.classList.remove('active')
+    })
+});
