255616d88fb42f6e5063dd7dea6fcc2b154fc7f9
isaaccodekill <isaacbello3200@gmail.com>
2019-08-05 11:35:39 +0100

    Create dropdowns, adjust sidenav logo, fixed external css

--- assets/js/close-img.js
+++ assets/js/close-img.js
@@ -1,13 +1,20 @@
 const closeImgbtn = document.querySelectorAll('.x-button')
 const overlayImage = document.querySelector('.overlay')
+const overlayerImage = document.querySelector(".overlayer")
 const cfm = document.querySelector(".withdrawal-confirmation")
 const shsucces = document.querySelectorAll(".notify-img")
 const clImg = document.querySelector(".close-img")
 
 closeImgbtn.forEach(button => {
 	button.addEventListener('click', (e) => {
-		overlayImage.classList.remove("show-form")
-		overlayImage.classList.remove("backgroundBlend")
+		
+		if(overlayerImage !== null){
+			overlayerImage.classList.remove("show-form")
+			overlayerImage.classList.remove('backgroundBlend')
+		}else{
+			overlayImage.classList.remove("show-form")
+			overlayImage.classList.remove("backgroundBlend")	
+		}
 		if(cfm !== null){
 			cfm.classList.remove("hideConfirmation")
 		}
