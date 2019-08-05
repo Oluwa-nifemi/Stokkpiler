011a899737728e6e3fdb4fef0f5c7d86a9ccd546
isaaccodekill <isaacbello3200@gmail.com>
2019-08-05 20:04:52 +0100

    Create the materials page -  Like a baws :-)

--- assets/js/withdraw.js
+++ assets/js/withdraw.js
@@ -5,7 +5,18 @@ const successImg = document.querySelector('.success-img')
 const failedImg = document.querySelector('.failed-img')
 const confirmBtn = document.querySelector('.confirm')
 const submit = document.querySelector('.plans-submit')
+const addBtn = document.querySelector('.btn__add')
 const closeImg = document.querySelector('.close-img')
+
+
+addBtn.addEventListener("click", (e) => {
+	e.preventDefault()
+    if(overlayer === null){
+	    overlay.classList.add('show-form')    	
+    }else{
+    	overlayer.classList.add('show-form')
+    }
+})
 
 submit.addEventListener("click", (e) => {
     e.preventDefault()
